e13b6d7907e1cf3655a95cfad63dd5c76203ce4b
SimantoTareq <tareq.hossain@northsouth.edu>
2023-01-11 02:11:03 +0600

    sing up with google

---
+++ delivery_app/lib/auth/sing_in.dart
@@ -0,0 +1,120 @@
+import 'dart:ffi';
+
+import 'package:delivery_app/homePage.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter_signin_button/flutter_signin_button.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+
+class SingIn extends StatefulWidget {
+  const SingIn({super.key});
+
+  @override
+  State<SingIn> createState() => _SingInState();
+}
+
+class _SingInState extends State<SingIn> {
+  _googleSignUp() async {
+    try {
+      final GoogleSignIn _googleSignIn = GoogleSignIn(
+        scopes: ['email'],
+      );
+      final FirebaseAuth _auth = FirebaseAuth.instance;
+
+      final GoogleSignInAccount? googleUser = await _googleSignIn.signIn();
+      final GoogleSignInAuthentication? googleAuth =
+          await googleUser?.authentication;
+
+      final AuthCredential credential = GoogleAuthProvider.credential(
+        accessToken: googleAuth?.accessToken,
+        idToken: googleAuth?.idToken,
+      );
+
+      final User? user = (await _auth.signInWithCredential(credential)).user;
+      //print("signed in " + user.displayName);
+
+      return user;
+    } catch (e) {
+      print(e.runtimeType);
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        height: double.infinity,
+        width: double.infinity,
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            fit: BoxFit.cover,
+            image: AssetImage('images/background.png'),
+          ),
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Container(
+              height: 400,
+              width: double.infinity,
+              // color: Colors.red,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Text('Sing in to contunue'),
+                  Text(
+                    'Vegi',
+                    style: TextStyle(
+                        fontSize: 50,
+                        color: Colors.white,
+                        shadows: [
+                          BoxShadow(
+                              blurRadius: 10,
+                              color: Colors.green.shade900,
+                              offset: Offset(3, 3))
+                        ]),
+                  ),
+                  Column(
+                    children: [
+                      SignInButton(
+                        Buttons.Google,
+                        text: "Sign in with Google",
+                        onPressed: () {
+                          _googleSignUp().then(
+                            (value) => Navigator.of(context).pushReplacement(
+                              MaterialPageRoute(
+                                builder: (context) => HomePage(),
+                              ),
+                            ),
+                          );
+                        },
+                      ),
+
+// with custom text
+                      SignInButton(
+                        Buttons.Facebook,
+                        text: "Sign in with Facebook",
+                        onPressed: () {},
+                      ),
+                    ],
+                  ),
+                  Column(
+                    children: [
+                      Text(
+                        'By singning in you are agreeing to our',
+                        style: TextStyle(color: Colors.grey.shade800),
+                      ),
+                      Text('Terms and Privac Policy'),
+                    ],
+                  )
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
